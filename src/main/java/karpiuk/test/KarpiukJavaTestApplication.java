d036d92219a971ff960e65a2c2ef23f8e2db0993
OleksandrCod <karpo99old@gmail.com>
2024-04-01 19:22:56 +0300

    initial commit

---
+++ src/main/java/karpiuk/test/KarpiukJavaTestApplication.java
@@ -0,0 +1,13 @@
+package karpiuk.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarpiukJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarpiukJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/karpiuk/test/KarpiukJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package karpiuk.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarpiukJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
